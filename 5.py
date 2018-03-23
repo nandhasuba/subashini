4e0baac80e1bfd087a09217f19cd62a5368f1955
nandhasuba <37650291+nandhasuba@users.noreply.github.com>
2018-03-23 16:16:25 +0530

    5
    greatest among three numbers

---
+++ 5.py
@@ -0,0 +1,11 @@
+a = float(input(" Enter the First value: "))
+b = float(input(" Enter the First value: "))
+c = float(input(" Enter the First value: "))
+ 
+if (a > b and a > c):
+          print("A is greater among three numbers")
+        
+elif (b > a and b > c):
+          print("B is greater among three numbers")
+else:
+          print("C is greater among three numbers")
