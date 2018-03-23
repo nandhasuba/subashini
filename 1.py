026397642adb2090b105066b3fd1f9fc97338b96
nandhasuba <37650291+nandhasuba@users.noreply.github.com>
2018-03-23 15:20:18 +0530

    1
    check the number

---
+++ 1.py
@@ -0,0 +1,9 @@
+n=int(input('Enter the number:'))
+if n>0:
+      print ("The given number is positive")
+elif  n<0:
+      print ("The given number is Negative")
+elif n==0:
+      print ("The given number is Zero")
+else:
+      print ("Invalid")
